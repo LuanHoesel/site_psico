92c110a3615c0cd627b7e51d07424e81aaf41faa
Luan Hoesel <luanhoesel@gmail.com>
2022-12-04 19:19:49 -0300

    telefone

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let inputNome = document.getElementById('nome');
 let inputEmail = document.getElementById('email');
 let inputTelefone = document.getElementById('telefone');
+let inputSenha = document.getElementById('senha');
 let botaoEnviar = document.getElementById('botaoEnviar');
 let clienteNome = document.getElementById("clienteNome");
 
@@ -18,10 +19,21 @@ if (botaoEnviar) {
             nome: inputNome.value,
             email: inputEmail.value,
             telefone: inputTelefone.value,
+            senha: inputSenha.value,
         
         };
-        salvaContaStorage(conta);
-        window.location.href = window.location.href.replace('login', 'agenda')
+        if (contaValida(conta)) {
+            salvaContaStorage(conta);
+            window.location.href = window.location.href.replace('login', 'agenda')
+        }
+    });
+}
+
+if(inputTelefone) {
+    inputTelefone.addEventListener('keyup', (evento) => {
+        inputTelefone.value = inputTelefone.value.replace(/[^0-9]+/g, '');
+        evento.preventDefault();
+        evento.stopPropagation();
     });
 }
 
@@ -38,4 +50,12 @@ function carregaLogin() {
         inputEmail.value = conta.email;
         inputTelefone.value = conta.telefone;
     }
+}
+function contaValida(conta) {
+    let temNome = !!conta.nome;
+    let temTelefone = !!conta.telefone;
+    let temEmail = !!conta.email;
+    let temSenha = !!conta.senha;
+    return temNome && temTelefone && temSenha && temEmail;
+
 }
